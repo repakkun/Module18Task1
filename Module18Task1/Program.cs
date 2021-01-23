4b069adb90b16cfc0faaf11fdbf5321a164b44d4
rePakkun <cj.ilya@yandex.ru>
2021-01-23 18:38:03 +0300

    Добавьте файлы проекта.

---
+++ Module18Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module18Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Введите два числа");
+            calculator.Addition(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            Console.ReadKey();
+        }
+        public interface ICalculator
+        {
+            void Addition(int a, int b);
+        }
+        public class Calculator : ICalculator
+        {           
+            public void Addition(int a, int b)
+            {
+                Console.WriteLine($"Сумма равна {a + b}");
+            }            
+        }
+    }
+}
